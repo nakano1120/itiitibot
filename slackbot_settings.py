5f0185fc7270e13e2a0451edb508dee11ab715e2
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2019-03-26 22:49:25 +0900

    Update slackbot_settings.py

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-17948093585-589200811190-TAI2EaoATOooIyOUShBQPctZ"
+API_TOKEN = "API_TOKEN"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "こんにちは"
