6021586591497c28d91091f3ecdae7cf47c2277b
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2019-03-27 20:07:08 +0900

    Update slackbot_settings.py

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -7,5 +7,8 @@ API_TOKEN = os.environ["SAMPLE_API_TOKEN"]
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "こんにちは"
 
+# 使用するPORT
+PORT = process.env.PORT || 5000;
+
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
 PLUGINS = ['plugins']
