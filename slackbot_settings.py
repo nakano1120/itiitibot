5be499351433be43f36e93817a1a6967a1bc0d3b
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2019-03-27 20:07:37 +0900

    Update slackbot_settings.py

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -8,7 +8,7 @@ API_TOKEN = os.environ["SAMPLE_API_TOKEN"]
 DEFAULT_REPLY = "こんにちは"
 
 # 使用するPORT
-PORT = process.env.PORT or 5000;
+PORT = process.env.PORT or 5000
 
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
 PLUGINS = ['plugins']
