bbbf7857a4a08e413a55f95404cf6cc6ee0204e0
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2019-05-12 00:12:50 +0900

    Add files via upload

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -27,8 +27,140 @@ from slackbot.bot import default_reply  # 該当する応答がない場合に�
 risten = 0
 answait = 0
 ans = '答え'
+yen = "2000"
+gas = {
+    "チーズONチーズINハンバーグ":799,
+    "チーズINハンバーグ":599,
+    "チーズINハンバーグ＆エビフライ":849,
+    "アボカドチーズINハンバーグ":849,
+    "ハンバーグ＆エビフライ":699,
+    "ハンバーグ＆チキン南蛮":799,
+    "てりたまハンバーグ":549,
+    "ミックスグリル":799,
+    "ハンバーグステーキ":499,
+    "肉盛り！ワイルドプレート":999,
+    "チキチキ！ピリ辛スパイス焼き！":699,
+    "若鶏のグリル　大葉おろしの醤油ソース":699,
+    "若鶏のグリル　ガーリックソース":699,
+    "若鶏の熟成醤油もろみ焼き":599,
+    "ビーフカットステーキ":999,
+    "ビーフカットステーキてんこもり":1299,
+    "若鶏と彩り野菜の黒酢あん":499,
+    "さばの味噌煮":399,
+    "豚肉の生姜焼き":599,
+    "焼き野菜と大葉おろしの和風ハンバーグ":599,
+    "ミックスフライ":699,
+    "若鶏の竜田揚げ　おろしポン酢":699,
+    "豚ロースのおろしとんかつ":599,
+    "マルゲリータピザ":599,
+    "シュリンプベーコンピザ":799,
+    "たっぷりマヨコーンピザ":499,
+    "なすとほうれん草のミートソーシスパゲティ":699,
+    "豆乳カルボナーラスパゲティ":699,
+    "海老のトマトソーススパゲティ＆根菜とカリカリチーズの20品目シーザーサラダSサイズセット":878,
+    "海老のトマトソーススパゲティ":599,
+    "トマトソーススパゲティ　モッツアレラチーズトッピング":649,
+    "トマトソーススパゲティ":499,
+    "ピリ辛肉味噌担担麺＆ご飯セット":858,
+    "ピリ辛肉味噌担々麺":699,
+    "ピリ辛肉味噌担々麺（糖質控えめ・ほうれん草麺）":799,
+    "1日分の野菜のベジ塩タンメン＆具だくさんフルーツヨーグルトセット":878,
+    "1日分の野菜のベジ塩タンメン":699,
+    "1日分の野菜のベジ塩タンメン（糖質控えめ・ほうれん草麺）":799,
+    "特製本格辛口チゲ（半玉うどん入り）＆十三穀米セット":878,
+    "特製本格辛口チゲ（半玉うどん入り）":699,
+    "海老と山芋オクラのねばとろサラダうどん＆ほうれん草ベーコンセット":878,
+    "海老と山芋オクラのねばとろサラダうどん":699,
+    "糖質控えめ・海老と山芋オクラのねばとろサラダ麺（糖質控えめ・ほうれん草麺）":799,
+    "オムライスビーフシチューソース＆ほうれん草ベーコンセット":878,
+    "オムライスビーフシチューソース":699,
+    "濃厚デミとさっぱりトマトのオムライス":799,
+    "温玉ミートドリア":499,
+    "ミートドリア":399,
+    "ネギトロ丼":899,
+    "ミニねぎとろ丼":449,
+    "ひれかつ丼（味噌汁・漬物付き）":699,
+    "海老ときのこの雑炊＆ちりめんじゃこと豆腐のねばとろサラダSサイズセット":928,
+    "海老とキノコの雑炊":599,
+    "おつまみコロッケ":199,
+    "揚げたてアジフライ":199,
+    "ちょい盛りポテトフライ":199,
+    "コーンのオーブン焼き":199,
+    "スナップエンドウとベーコンのオーブン焼き":199,
+    "ソーセージグリル":199,
+    "ほうれん草ベーコン":199,
+    "若鶏の唐揚げ（５コ）":299,
+    "山盛りポテトフライ":299,
+    "豚肉てんこもやし":299,
+    "海老とアボカドのタルタル仕立て":299,
+    "トマトとモッツァレラのカプレーゼ風":299,
+    "チキンとモッツァレラのトマトオーブン焼き":299,
+    "ミニ牛タンシチュー":399,
+    "トウモロコシのポタージュ":199,
+    "ソフトフランスパン":179,
+    "グリーンスムージー":299,
+    "セットドリンクバー":219,
+    "ストロベリーソースのベリーベリーパンケーキ":599,
+    "チョコバナナとマスカルポーネのパンケーキ":599,
+    "パリッと食感のチョコレートパフェ":499,
+    "チョコバナナサンデー":399,
+    "フォンダンショコラ　ソフトクリームトッピング":399,
+    "フォンダンショコラ":299,
+    "ソフトクリームあんみつ":399,
+    "お濃い抹茶ババロア白玉あずきの盛り合わせ":399,
+    "お濃い抹茶ババロア":199,
+    "ぷるぷる黒糖ゼリー":199,
+    "ぷるぷる黒糖ゼリー":299,
+    "ソフトクリーム":279,
+    "ソフトクリーム（ストロベリーソース）":299,
+    "ソフトクリーム（黒糖きなこ）":299,
+    "ベイクドチーズケーキ":299,
+    "具沢山フルーツヨーグルト":199,
+    "ライス":179,
+    "十三穀米":229,
+    "味噌汁":159,
+    "日替わりスープ":159,
+    "納豆":79,
+    "とろろ":100,
+}
+@listen_to('ガスト1000')
+def gast0_func(message):
+    now = 0
+    message.send('今日のガストガチャ(1000円コース)')
+    while now < 927:
+        if now > 725:
+            zeikomi = int(now * 1.08)
+            message.send(">合計 ¥"+ str(zeikomi) + " (税抜 ¥"+ str(now) + " )")
+            break
+        tabe = random.choice(list(gas.keys()))
+        price = int(gas[tabe])
+        if now + price < 927:
+            now += price
+            onezei = int(price*1.08)
+
+            message.send(tabe + " ¥" + str(onezei))
+
+@listen_to('ガスト1500')
+def gast1_func(message):
+    now = 0
+    message.send('今日のガストガチャ(1500円コース)')
+    while now < 1390:
+        if now > 1188:
+            zeikomi = int(now * 1.08)
+            message.send(">合計 ¥"+ str(zeikomi) + " (税抜 ¥"+ str(now) + " )")
+            break
+        tabe = random.choice(list(gas.keys()))
+        price = int(gas[tabe])
+        if now + price < 1390:
+            now += price
+            onezei = int(price*1.08)
+
+            message.send(tabe + " ¥" + str(onezei))
+
 @listen_to('quiz0')
 def quiz0_func(message):
+    message.send("本業はちょっとお休みします。")
+    return
     global risten
     global ans
     global dd
@@ -44,14 +176,14 @@ def quiz0_func(message):
     ans = dd[monban]
     f.close()
         
-@listen_to('help')
+@listen_to('!help')
 def help_func(message):
-    message.send('quiz◯と入力すると問題が出題されます。')
+    message.send('ガスト1000 または　ガスト1500 でガストのメニューを作ります。')
     message.send('quiz0:クイズ quiz1:物理 quiz2:化学 quiz3:生物 quiz4:日本史 quiz5:世界史 quiz6:地理')
     message.send('回答するときは、「a」を入力し、回答権を得たら「a:答え」のように入力します。')
-    message.send('問題を飛ばしたいときは、「pass」と打ちます。')
+    message.send('問題を飛ばしたいときは、「!pass」と打ちます。')
 
-@listen_to('pass')
+@listen_to('!pass')
 def pas_func(message):
     global risten
     message.send('問題をパスします。')
@@ -87,49 +219,6 @@ def kaito_func(message):
         message.reply('残念。正解は ' + ans)
     answait = 0
     risten = 0
-
-@respond_to('a')
-def ranswer_func(message):
-    global answait
-    global ansuser
-    if risten == 0:
-        return   
-    message.reply('答えの前に qq を付けてチャットで回答してください。')
-    answait = 1
-
-@respond_to(r'^qq')
-def rkaito_func(message):
-    global anuser
-    global answait
-    global risten
-    if answait == 0:
-        return
-    anuser = message.body['user']
-    text = message.body['text']
-    textet = text.replace('qq', '')
-    if textet == ans:
-        message.reply('正解です')
-
-    elif textet != ans:
-        message.reply('残念。正解は ' + ans)
-    answait = 0
-    risten = 0       
-        
-@respond_to('help')
-def rhelp_func(message):
-    message.send('quiz◯と入力すると問題が出題されます。')
-    message.send('quiz0:クイズ quiz1:物理 quiz2:化学 quiz3:生物 quiz4:日本史 quiz5:世界史 quiz6:地理')
-    message.send('回答するときは、「a」を入力し、回答権を得たら「a:答え」のように入力します。')
-    message.send('問題を飛ばしたいときは、「pass」と打ちます。')
-    message.send('')
-
-@respond_to('pass')
-def rpas_func(message):
-    if answait == 0:
-        return
-    global risten
-    message.send('問題をパスします。')
-    risten = 0
 #ここまでquiz機能
 #ここからおまけ
 @respond_to('spnews')
@@ -148,10 +237,6 @@ def rspnews_func(message):
         message.send(link)
         message.send(title)
         break
-    userid = message.body['user']
-    f = open('plugins/quiz00.json','r')
-    df = json.load(f)
-    f.close()
 
 @respond_to('itnews')
 def ritnews_func(message):
@@ -169,10 +254,6 @@ def ritnews_func(message):
         message.send(link)
         message.send(title)
         break
-    userid = message.body['user']
-    f = open('plugins/quiz00.json','r')
-    df = json.load(f)
-    f.close()
 
 @respond_to('mainnews')
 def rmainnews_func(message):
@@ -190,7 +271,37 @@ def rmainnews_func(message):
         message.send(link)
         message.send(title)
         break
-    userid = message.body['user']
-    f = open('plugins/quiz00.json','r')
-    df = json.load(f)
-    f.close()
+@respond_to('ガスト1000')
+def gastr_func(message):
+    now = 0
+    message.send('今日のガストガチャ(1000円コース)')
+    while now < 927:
+        if now > 725:
+            zeikomi = int(now * 1.08)
+            message.send(">合計 ¥"+ str(zeikomi) + " (税抜 ¥"+ str(now) + " )")
+            break
+        tabe = random.choice(list(gas.keys()))
+        price = int(gas[tabe])
+        if now + price < 927:
+            now += price
+            onezei = int(price*1.08)
+
+            message.send(tabe + " ¥" + str(onezei))
+
+@respond_to('ガスト1500')
+def gastrr_func(message):
+    now = 0
+    message.send('今日のガストガチャ(1500円コース)')
+    while now < 1390:
+        if now > 1188:
+            zeikomi = int(now * 1.08)
+            message.send(">合計 ¥"+ str(zeikomi) + " (税抜 ¥"+ str(now) + " )")
+            break
+        tabe = random.choice(list(gas.keys()))
+        price = int(gas[tabe])
+        if now + price < 1390:
+            now += price
+            onezei = int(price*1.08)
+
+            message.send(tabe + " ¥" + str(onezei))
+
